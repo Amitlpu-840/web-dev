81dbd0648d53d1f466c5b872181aeb335ebc5df0
Amit Giri <thisisag840@gmail.com>
2022-07-31 20:03:20 +0530

    color generator completed

--- Random Color generator/index.js
+++ Random Color generator/index.js
@@ -1,6 +1,5 @@
 const containerEl = document.querySelector(".container");
 
-const colorContainerEls = document.querySelectorAll(".color-container");
 
 
 for (let index = 0; index < 30; index++) {
@@ -8,18 +7,24 @@ for (let index = 0; index < 30; index++) {
     colorContainerEl.classList.add("color-container");
     containerEl.appendChild(colorContainerEl);
 }
-generateColors();
-function generateColors() {
-    colorContainerEls = forEach((colorContainerEl) => {
-        const newColorCode = randomColor();
-        console.log(newColorCode);
 
+const colorContainerEls = document.querySelectorAll(".color-container");
+// console.log(colorContainerEls);
+
+generateColors();
+
+function generateColors() {
+    colorContainerEls.forEach((colorContainerEl) => {
+        const newColorCode = randomColor();
+
+        colorContainerEl.style.backgroundColor = '#' + newColorCode;
+        colorContainerEl.innerText = '#' + newColorCode;
     });
 }
 
 
 
-randomColor();
+//randomColor();
 
 function randomColor() {
     const chars = "0123456789abcdef";
@@ -27,9 +32,14 @@ function randomColor() {
     let colorCode = "";
     for (let index = 0; index < colorCodeLength; index++) {
         const randomNum = Math.floor(Math.random() * chars.length);
+
+
         colorCode += chars.substring(randomNum, randomNum + 1);
-        return colorCode;
+        //  console.log(colorCode, randomNum)
+
 
 
     }
+    return colorCode;
+
 }
---
+++ Random Color generator/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+  // for (let index = 0; index < colorCodeLength; index++) {
+    //     const randomNum = Math.floor(Math.random() * chars.length);
+    //     colorCode += chars.substring(randomNum, randomNum + 1);
+    //     return colorCode;
+
+
+    // }
